b4dd5600158a2086b97e37eaab951bbda2f891f1
johaljicar@hotmail.com <johaljicar@hotmail.com>
2019-04-15 21:51:32 -0500

    adding languages according to document's last number

---
+++ Votings/Votings.UI/Votings.UI/Infrastructure/InstanceLocator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Votings.UI.ViewModels;
+
+namespace Votings.UI.Infrastructure
+{
+    public class InstanceLocator
+    {
+        public MainViewModel Main { get; set; }
+
+        public InstanceLocator()
+        {
+            this.Main = new MainViewModel();
+        }
+    }
+}
---
+++ Votings/Votings.UI/Votings.UI/ViewModels/MainViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Votings.UI.ViewModels
+{
+    public class MainViewModel
+    {
+        private static MainViewModel instance;
+    }
+}
